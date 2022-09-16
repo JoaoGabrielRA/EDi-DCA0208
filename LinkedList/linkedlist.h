bba62f22fdc1af00859a7fdb28347a9f8fab5ab7
João Gabriel <71520531+JoaoGabrielRA@users.noreply.github.com>
2022-09-16 10:44:24 -0300

    Update linkedlist.h
    Correção alocação novos nós

--- LinkedList/linkedlist.h
+++ LinkedList/linkedlist.h
@@ -21,85 +21,90 @@ struct linkedlist *inicializar() {
 }
 
 struct no *alocarNovoNo(int valor) {
-  // TODO
+  struct no *novoNo = (struct no *)malloc(sizeof(struct no));
+  novoNo->val = valor;
+  novoNo->prox = 0;
+  return novoNo;
 }
 
 void inserirElementoNoFim(struct linkedlist *listaligada, int valor) {
   if (listaligada->qtdade == 0) {
-    listaligada->cabeca = (struct no *)malloc(sizeof(struct no)); //aloca primeiro nó;
-    
-    listaligada->cabeca->prox = 0; //atribui valor para o 0 para a "ultima" cabeca do nó;
-    listaligada->cabeca->val = valor; //atribui o valor para o nó;
+    listaligada->cabeca = alocarNovoNo(valor); // aloca primeiro nó;
   } else {
-    struct no *aux = listaligada->cabeca; //guarda o valor do proximo no;
-    for (int i = 1; i < listaligada->qtdade; i++) {
+    struct no *aux = listaligada->cabeca; // guarda o valor da cabeca;
+    for (int i = 0; i < listaligada->qtdade - 1; i++) {
       aux = aux->prox;
-    } //caminha até a ultima posicao;
-    aux->prox = (struct no *)malloc(sizeof(struct no));
-    aux->prox->prox = 0;
-    aux->prox->val = valor;
+      // caminha até a penultima posicao;
+    }
+    aux->prox = alocarNovoNo(valor);
   }
   listaligada->qtdade++;
 }
 
 void inserirElementoNoInicio(struct linkedlist *listaligada, int valor) {
-  if(listaligada->qtdade <=0){
-  printf("array vazio!");
-    exit(0);
-  }
-  else{
-    for(int i=0; i<1 ;i++){
-      listaligada->cabeca->val = valor;
-    }
-  }
+
+  struct no *novoNo = alocarNovoNo(valor);
+  novoNo->prox = listaligada->cabeca;
+  listaligada->cabeca = novoNo;
+
+  listaligada->qtdade++;
 }
 
-void inserirElementoEmPosicao(struct linkedlist *listaligada, int valor,int posicao) {
-  if(posicao >= listaligada->qtdade || posicao < 0){
+void inserirElementoEmPosicao(struct linkedlist *listaligada, int valor,
+                              int posicao) {
+  if (posicao >= listaligada->qtdade || posicao < 0) {
     printf("posicao inválida");
     exit(0);
   }
-  else{
-      struct no *aux = listaligada->cabeca;
-      for(int i=0; i<posicao;i++){
-        aux = aux->prox;
-      }
-    aux->val = valor;
+  if (posicao == 0) {
+    inserirElementoNoInicio(listaligada, valor);
+  } else {
+    struct no *aux = listaligada->cabeca;
+    for (int i = 0; i < posicao-1; i++) {
+      aux = aux->prox;
+    }
+    struct no *NovoNo = alocarNovoNo(valor);
+    NovoNo->prox = aux->prox;
+    aux->prox = NovoNo;
+    listaligada->qtdade++;
   }
 }
 
 int obterElementoEmPosicao(struct linkedlist *listaligada, int posicao) {
-  if(posicao >= listaligada->qtdade || posicao < 0){
+  if (posicao > listaligada->qtdade || posicao < 0) {
     printf("posicao inválida");
     exit(0);
-  }
-  else{
-      struct no *aux = listaligada->cabeca;
-      for(int i=0; i < posicao; i++){
+  } else {
+    if (posicao == 0) {
+      printf("O valor na posicao %d é %d", posicao, listaligada->cabeca->val);
+    }
+    struct no *aux = listaligada->cabeca;
+    for (int i = 0; i < posicao; i++) {
       aux = aux->prox;
-      }
-    printf("%d: ", aux->val);
+    }
+    printf("O valor na posição %d corresponde a %d: ", posicao, aux->val);
   }
 }
 
 void removerElementoEmPosicao(struct linkedlist *listaligada, int posicao) {
-  if(posicao > listaligada->qtdade || posicao <0){
+  if (posicao > listaligada->qtdade || posicao < 0) {
     printf("Posição Inválida");
     exit(0);
-  }
-  else{
-       struct no* aux = listaligada->cabeca;
+  } else {
+    struct no *aux = listaligada->cabeca;
     if (posicao == 0) {
-        listaligada->cabeca = listaligada->cabeca->prox;
-        free(aux);
-        listaligada->qtdade--;
-        return;
+      listaligada->cabeca = listaligada->cabeca->prox;
+      free(aux);
+      listaligada->qtdade--;
     }
-    for (int i = 1; i < posicao; i++) {
-        aux = aux->prox;
+    for (int i = 0; i < posicao - 1; i++) {
+      aux = aux->prox;
     }
-    struct no* aux2 = aux->prox;
-    aux->prox = aux->prox->prox;
+    struct no *aux2 = aux->prox;
+    aux->prox = aux->prox->prox; // recebe 0;
+
+    printf("Elementos com valor %d removido \n" , aux2->val);
+
     free(aux2);
     listaligada->qtdade--;
   }
