6e58812927264b1c5ac61b04e30150ef6752f512
João Gabriel <71520531+JoaoGabrielRA@users.noreply.github.com>
2022-09-16 10:45:57 -0300

    Update main.c
    Testes de funcionalidades de Linkedlist

--- LinkedList/main.c
+++ LinkedList/main.c
@@ -6,23 +6,61 @@
 int main() {
 
   struct linkedlist *listaligada = inicializar();
-  inserirElementoNoFim(listaligada, 10);
-  inserirElementoNoFim(listaligada, 127);
-  inserirElementoNoFim(listaligada, 115);
+
+  inserirElementoNoFim(listaligada, 1);
+  printf("foi - ");
+  imprimirlistaligada(listaligada);
+
+  printf("\n");
+
+  inserirElementoNoFim(listaligada, 2);
+  printf("foi - ");
+  imprimirlistaligada(listaligada);
+
+  printf("\n");
+
+  inserirElementoNoFim(listaligada, 3);
+  printf("foi - ");
   imprimirlistaligada(listaligada);
   printf("\n");
-  imprimirlistaligada(listaligada);
-  printf("\n");
+
   inserirElementoNoInicio(listaligada, 5);
-  printf("\n");
+
+  printf("foi - ");
   imprimirlistaligada(listaligada);
   printf("\n");
-  inserirElementoEmPosicao(listaligada, 25, 2); 
+  printf("quantidade de elementos em cima: %d", listaligada->qtdade);
+  printf("\n");
+
+  inserirElementoEmPosicao(listaligada, 25, 0);
+  printf("foi - ");
   imprimirlistaligada(listaligada);
   printf("\n");
-  //obterElementoEmPosicao(listaligada, 1);
-  removerElementoEmPosicao(listaligada,2);
+  printf("quantidade de elementos em cima: %d", listaligada->qtdade);
+  printf("\n");
+
+  inserirElementoEmPosicao(listaligada, 22, 4);
+  printf("foi - ");
   imprimirlistaligada(listaligada);
+  printf("foi - ");
+  printf("\n");
+  printf("quantidade de elementos: %d", listaligada->qtdade);
+  printf("\n");
+
+  obterElementoEmPosicao(listaligada, 2);
+  printf("\n");
+  
+  removerElementoEmPosicao(listaligada, 2);
+  imprimirlistaligada(listaligada);
+  printf("\n");
+  printf("quantidade de elementos em cima: %d", listaligada->qtdade);
+  printf("\n");
+
+  inserirElementoNoInicio(listaligada,7);
+  imprimirlistaligada(listaligada);
+  printf("\n");
+  printf("quantidade de elementos em cima: %d", listaligada->qtdade);
+  
   free(listaligada);
   return 0;
 }
