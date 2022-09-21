b52cfa4267c9e74bd5b2f389100f5687778abb67
João Gabriel <71520531+JoaoGabrielRA@users.noreply.github.com>
2022-09-20 21:05:03 -0300

    DoublyLinkedList
    Implementação da ED

---
+++ doublyLinkedList/doubly.h
@@ -0,0 +1,120 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct no {
+  struct no *ant;
+  int val;
+  struct no *prox;
+};
+
+struct doublylista {
+  struct no *cabeca;
+  struct no *cauda;
+  int qtdade;
+};
+
+struct doublylista *inicializar() {
+  struct doublylista *lista =
+      (struct doublylista *)malloc(sizeof(struct doublylista));
+  lista->cabeca = 0;
+  lista->cauda = 0;
+  lista->qtdade = 0;
+  return lista;
+}
+
+struct no *alocarNovoNo(int valor) {
+  struct no *novoNo = (struct no *)malloc(sizeof(struct no));
+  novoNo->val = valor;
+  novoNo->ant = 0;
+  novoNo->prox = 0;
+}
+
+void inserirElementoNoInicio(struct doublylista *lista, int valor) {
+  struct no *novoNo = alocarNovoNo(valor);
+  if (lista->qtdade == 0) {
+    lista->cabeca = novoNo;
+    lista->cauda = novoNo;
+  } else {
+    struct no *aux = lista->cabeca;
+    lista->cabeca = novoNo;
+    novoNo->prox = aux;
+  }
+  lista->qtdade++;
+  printf("Valor %d inserido no inicio\n", valor);
+}
+
+void inserirElementoNoFim(struct doublylista *lista, int valor) {
+  struct no *novoNo = alocarNovoNo(valor);
+  if (lista->qtdade == 0) {
+    inserirElementoNoInicio(lista, valor);
+  } else {
+    struct no *aux = lista->cauda;
+    lista->cauda = novoNo;
+    novoNo->ant = aux;
+    aux->prox = novoNo;
+  }
+  lista->qtdade++;
+  printf("Valor %d inserido no fim\n", valor);
+}
+
+void inserirElementoEmPosicao(struct doublylista *lista, int valor,int posicao) {
+  if (posicao > lista->qtdade || posicao < 0) {
+    printf("POSICAO INVALIDA!");
+  }
+  if (lista->qtdade == 0) {
+    inserirElementoNoInicio(lista, valor);
+  } else {
+    struct no *novoNo = alocarNovoNo(valor);
+    struct no *aux = lista->cabeca;
+    for (int i = 0; i < posicao - 1; i++) {
+      aux = aux->prox;
+    }
+    novoNo->ant = aux;
+    novoNo->prox = aux->prox;
+    aux->prox = novoNo;
+  }
+  printf("Valor %d inserido na posicao %d\n", valor,posicao);
+  lista->qtdade++;
+
+}
+
+int obterElementoEmPosicao(struct doublylista *lista, int posicao) {
+  if(posicao > (lista->qtdade) || posicao <0){
+    printf("Posição inválida");
+  }
+    struct no *aux = lista->cabeca;
+    for(int i=0 ; i<posicao ;i++){
+      aux = aux->prox;
+    }
+    printf("O valor da posicao %d corresponde a %d", posicao, aux->val);
+}
+
+void removerElementoEmPosicao(struct doublylista *lista, int posicao) {
+  struct no *aux = lista->cabeca;
+  for(int i = 0; i <posicao ;i++){
+      aux = aux->prox;
+  }
+  aux->ant->prox = aux->prox;
+  aux->prox->ant = aux->ant;
+  printf("Elemento removido da posicao %d\n",posicao);
+  free(aux);
+}
+
+void imprimirLista(struct no **cabeca) {
+  // usamos o aux para percorrer a lista
+  if ((*cabeca) != NULL) {
+    struct no *aux = (*cabeca);
+    // navega partindo da cabeça até chegar NULL
+    printf("[");
+    do {
+      printf("%d", aux->val);
+      aux = aux->prox;
+      if (aux != NULL) {
+        printf(", ");
+      }
+    } while (aux != NULL);
+    printf("]");
+  } else {
+    printf("A lista está vazia!");
+  }
+}
---
+++ doublyLinkedList/main.c
@@ -0,0 +1,33 @@
+#include "doubly.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+  struct doublylista *lista = inicializar();
+
+  inserirElementoNoInicio(lista, 1);
+  inserirElementoNoInicio(lista, 3);
+  inserirElementoNoInicio(lista, 4);
+  imprimirLista(lista);
+  printf("\n\n");
+
+  inserirElementoNoFim(lista, 5);
+  inserirElementoNoFim(lista, 8);
+  imprimirLista(lista);
+  printf("\n\n");
+
+  inserirElementoEmPosicao(lista, 69, 1);
+  imprimirLista(lista);
+  printf("\n\n");
+
+  obterElementoEmPosicao(lista,4);
+  printf("\n\n");
+
+  removerElementoEmPosicao(lista,4);
+  printf("\n\n");
+  imprimirLista(lista);
+
+  free(lista);
+}
